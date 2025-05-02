5f013933b9a56f3d69924d5b2f74d9489a9c44ca
kkyouma <144501527+kkyouma@users.noreply.github.com>
2025-05-01 22:21:15 -0400

    Add LaTeX support

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   output: "static",
   prefetch: true,
   compressHTML: true,
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
+
